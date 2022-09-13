1cb787ffed51a18d9992ac7074c99d267e40f8d1
ManuRodriguezC <estebanclimb@gmail.com>
2022-09-12 21:24:16 -0500

    1 and 2 days in the learning of python

---
+++ day_2/variables.py
@@ -0,0 +1,70 @@
+#Day 2:30 Days of python programming
+
+#Exercises: Level 1
+
+firts_name = "Esteban"
+last_name = "Rodriguez"
+full_name = "Esteban Rodriguez"
+country = "Colombia"
+city = "Madrid"
+age = 27
+year = 2022
+is_married = True
+is_light = True
+
+#Exercises: Level 2
+print("\n")
+print("------ Types of the variables ------\n")
+print('-- firts_name -- is a variable of type: ', type(firts_name))
+print("-- last_name --- is a variable of type: ", type(last_name))
+print("-- full_name --- is a variable of typy: ", type(full_name))
+print("-- country ----- is a variable of typy: ", type(country))
+print("----- city ----- is a variable of typy: ", type(city))
+print("------ age ----- is a variable of typy: ", type(age))
+print("----- year ----- is a variable of typy: ", type(year))
+print("-- is_married -- is a variable of typy: ", type(is_married))
+print("--- is_light --- is a variable of typy: ", type(is_light))
+print("\n")
+
+print("The length of firts_name is : ", len(last_name))
+print("\n")
+print("The differents of the length between firts_name and last_name is: ", len(firts_name) - len(last_name))
+print("\n")
+
+num_one = 5
+num_two = 4
+
+add = num_one + num_two
+sub = num_one - num_two
+mul = num_one * num_two
+div = num_one / num_two
+mod = num_one % num_two
+exp = num_one ** num_two
+floor = num_one // num_two
+
+print("The num_one is: ", num_one)
+print("The num_two is: ", num_two)
+print("\n")
+print("The Add of num_one and num_two is :", add)
+print("The Subtract of num_one and num_two is :", sub)
+print("Divide of num_one and num_two is :", div)
+print("Modulo of num_one and num_two is :", mod)
+print("Exponent of num_one and num_two is :", exp)
+print("Floor div of the num_one and num_two is :", floor)
+
+print("\n")
+
+print("The radius of a circle is 30 meters")
+radius = 30
+pi = 3.1416
+area_of_circle = pi * radius**2
+circum_of_circle = radius * 2 * pi
+print("The area od the circle is :", area_of_circle) 
+print("The circumference of the circle is :", circum_of_circle)
+
+print("\n")
+
+name = input('What is your name: ')
+last_name = input('What is your last name: ')
+country = input('What is your country: ')
+age = input('What is your age: ')
